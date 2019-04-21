0ce3a1601b56658270ec5b3198e7272c0d64b1a0
maendros <valitsianius@hotmail.com>
2019-04-22 00:00:13 +0300

    implementing api and fetchcategories action and reducer

--- src/utils/ReadableAPI.js
+++ src/utils/ReadableAPI.js
@@ -14,141 +14,141 @@ export const fetchCategories = () =>
     .then(res => res.json())
     .then(data => data.categories);
 
-// export const fetchPosts = category => {
-//   const url = category ? `${api}/${category}/posts` : `${api}/posts`;
-//   fetch(url, { headers })
-//     .then(res => res.json())
-//     .then(data => data);
-// };
-// //POSTS
-// export const fetchPost = id => {
-//   fetch(`${api}/posts/${id}`, { headers })
-//     .then(res => res.json())
-//     .then(data => data);
-// };
-// export const addPost = post => {
-//   const data = {
-//     ...post,
-//     timestamp: Date.now()
-//   };
-//   fetch(`${api}/posts`, {
-//     method: "POST",
-//     headers: {
-//       ...headers,
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify({ data })
-//   })
-//     .then(res => res.json())
-//     .then(data => data);
-// };
+export const fetchPosts = category => {
+  const url = category ? `${api}/${category}/posts` : `${api}/posts`;
+  fetch(url, { headers })
+    .then(res => res.json())
+    .then(data => data);
+};
+//POSTS
+export const fetchPost = id => {
+  fetch(`${api}/posts/${id}`, { headers })
+    .then(res => res.json())
+    .then(data => data);
+};
+export const addPost = post => {
+  const data = {
+    ...post,
+    timestamp: Date.now()
+  };
+  fetch(`${api}/posts`, {
+    method: "POST",
+    headers: {
+      ...headers,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ data })
+  })
+    .then(res => res.json())
+    .then(data => data);
+};
 
-// export const votePost = (id, option) =>
-//   fetch(`${api}/posts/${id}`, {
-//     method: "POST",
-//     headers: {
-//       ...headers,
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify({ option })
-//   })
-//     .then(res => res.json())
-//     .then(data => data);
+export const votePost = (id, option) =>
+  fetch(`${api}/posts/${id}`, {
+    method: "POST",
+    headers: {
+      ...headers,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ option })
+  })
+    .then(res => res.json())
+    .then(data => data);
 
-// export const updatePost = post => {
-//   const data = {
-//     ...post,
-//     timestamp: Date.now()
-//   };
-//   fetch(`${api}/posts/${post.id}`, {
-//     method: "PUT",
-//     headers: {
-//       ...headers,
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify({ data })
-//   })
-//     .then(res => res.json())
-//     .then(data => data);
-// };
+export const updatePost = post => {
+  const data = {
+    ...post,
+    timestamp: Date.now()
+  };
+  fetch(`${api}/posts/${post.id}`, {
+    method: "PUT",
+    headers: {
+      ...headers,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ data })
+  })
+    .then(res => res.json())
+    .then(data => data);
+};
 
-// export const deletePost = post =>
-//   fetch(`${api}/posts/${post.id}`, {
-//     method: "DELETE",
-//     headers: {
-//       ...headers,
-//       "Content-Type": "application/json"
-//     }
-//   })
-//     .then(res => res.json())
-//     .then(data => data);
+export const deletePost = post =>
+  fetch(`${api}/posts/${post.id}`, {
+    method: "DELETE",
+    headers: {
+      ...headers,
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(data => data);
 
-// //Comments
+//Comments
 
-// export const fetchComments = id =>
-//   fetch(`${api}/posts/${id}/comments`, { headers })
-//     .then(res => res.json())
-//     .then(data => data);
+export const fetchComments = id =>
+  fetch(`${api}/posts/${id}/comments`, { headers })
+    .then(res => res.json())
+    .then(data => data);
 
-// //POSTS
-// export const fetchComment = id =>
-//   fetch(`${api}/comments/${id}`, { headers })
-//     .then(res => res.json())
-//     .then(data => data);
+//POSTS
+export const fetchComment = id =>
+  fetch(`${api}/comments/${id}`, { headers })
+    .then(res => res.json())
+    .then(data => data);
 
-// export const addComment = comment => {
-//   const data = {
-//     ...comment,
-//     timestamp: Date.now()
-//   };
-//   fetch(`${api}/comments`, {
-//     method: "POST",
-//     headers: {
-//       ...headers,
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify({ data })
-//   })
-//     .then(res => res.json())
-//     .then(data => data);
-// };
+export const addComment = comment => {
+  const data = {
+    ...comment,
+    timestamp: Date.now()
+  };
+  fetch(`${api}/comments`, {
+    method: "POST",
+    headers: {
+      ...headers,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ data })
+  })
+    .then(res => res.json())
+    .then(data => data);
+};
 
-// export const voteComment = (id, option) =>
-//   fetch(`${api}/comments/${id}`, {
-//     method: "POST",
-//     headers: {
-//       ...headers,
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify({ option })
-//   })
-//     .then(res => res.json())
-//     .then(data => data);
+export const voteComment = (id, option) =>
+  fetch(`${api}/comments/${id}`, {
+    method: "POST",
+    headers: {
+      ...headers,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ option })
+  })
+    .then(res => res.json())
+    .then(data => data);
 
-// export const updateComment = comment => {
-//   const data = {
-//     ...comment,
-//     timestamp: Date.now()
-//   };
-//   fetch(`${api}/comments/${comment.id}`, {
-//     method: "PUT",
-//     headers: {
-//       ...headers,
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify({ data })
-//   })
-//     .then(res => res.json())
-//     .then(data => data);
-// };
+export const updateComment = comment => {
+  const data = {
+    ...comment,
+    timestamp: Date.now()
+  };
+  fetch(`${api}/comments/${comment.id}`, {
+    method: "PUT",
+    headers: {
+      ...headers,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ data })
+  })
+    .then(res => res.json())
+    .then(data => data);
+};
 
-// export const removeComment = comment =>
-//   fetch(`${api}/comments/${comment.id}`, {
-//     method: "DELETE",
-//     headers: {
-//       ...headers,
-//       "Content-Type": "application/json"
-//     }
-//   })
-//     .then(res => res.json())
-//     .then(data => data);
+export const removeComment = comment =>
+  fetch(`${api}/comments/${comment.id}`, {
+    method: "DELETE",
+    headers: {
+      ...headers,
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(data => data);
